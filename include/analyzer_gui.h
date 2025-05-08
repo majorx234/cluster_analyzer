cf9650122de55ae436dd0e627d27bb117dff9278
MajorX234 <majorx234@googlemail.com>
2025-05-08 23:52:53 +0200

    Add wdiget to display cluster

--- include/analyzer_gui.h
+++ include/analyzer_gui.h
@@ -6,5 +6,13 @@
 #include <float.h>
 #include "data.h"
 
+typedef struct UiRect {
+  float x;
+  float y;
+  float w;
+  float h;
+} UiRect;
+
 Color get_color(size_t idx);
 int start_render_loop();
+void cluster_widget(UiRect r, Vector2 *set, Vector2 *clusters[], Vector2 means[], Limits limits);
--- src/analyzer_gui.c
+++ src/analyzer_gui.c
@@ -9,6 +9,11 @@
 #include "stb_ds.h"
 #include "analyzer_gui.h"
 
+#define ARRAY_LEN(xs) sizeof(xs)/sizeof(xs[0])
+
+#define SAMPLE_RADIUS 2
+#define CENTROID_RADIUS 5
+
 Color get_color(size_t idx){
   Color colors[] = {
     PINK,
@@ -25,7 +30,20 @@ Color get_color(size_t idx){
     BEIGE,
     BROWN,
   };
-  return colors[idx];
+  return colors[idx%(ARRAY_LEN(colors))];
+}
+
+Vector2 project_sample_to_screen(UiRect r, Vector2 sample, Limits limits)
+{
+  // normalize to 0..1
+  float nx = (sample.x - limits.min_x)/(limits.max_x - limits.min_x);
+  float ny = (sample.y - limits.min_y)/(limits.max_y - limits.min_y);
+
+  // project to screen
+  return CLITERAL(Vector2) {
+    .x = r.w*nx + r.x,
+    .y = r.h - (r.h*ny) + r.y,
+  };
 }
 
 int start_render_loop() {
@@ -49,4 +67,22 @@ int start_render_loop() {
   }
   CloseWindow();
   return 0;
+}
+
+void cluster_widget(UiRect r, Vector2 *set, Vector2 *clusters[], Vector2 means[], Limits limits)
+{
+    for (size_t i = 0; i < arrlen(set); ++i) {
+        Vector2 it = set[i];
+        DrawCircleV(project_sample_to_screen(r, it, limits), SAMPLE_RADIUS, LIGHTGRAY);
+    }
+    for (size_t i = 0; i < 3; ++i) {
+      Color color = get_color(i);
+
+        for (size_t j = 0; j < arrlen(clusters[i]); ++j) {
+            Vector2 it = clusters[i][j];
+            DrawCircleV(project_sample_to_screen(r, it, limits), SAMPLE_RADIUS, color);
+        }
+
+        DrawCircleV(project_sample_to_screen(r, means[i], limits), CENTROID_RADIUS, color);
+    }
 }
