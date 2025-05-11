5f90b6853a223f9801988eb292166938a22591a7
MajorX234 <majorx234@googlemail.com>
2025-05-12 01:04:04 +0200

    switch to macro based lerp

--- include/extra_math.h
+++ include/extra_math.h
@@ -4,8 +4,7 @@
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
-inline float lerp(float a, float b, float ratio) {
-  return a + ratio*(a-b);
-}
+#define lerp(a, b, ratio) ( a + ratio*(a-b))
+
 
 #endif // EXTRA_MATH_H_
